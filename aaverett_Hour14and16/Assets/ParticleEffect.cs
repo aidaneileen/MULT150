d55303aa102f28707080290fbb2dcb592acb2e19
straysheep14 <aidaneileen123@gmail.com>
2025-11-22 20:14:52 -0800

    Hour 14 and 16 Complete

---
+++ aaverett_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,28 @@
+/*
+ParticleEffect modifications for aaverett_Hour16
+
+1. Main
+   - Duration: 3.
+   - Start Speed: 0.5.
+   - Start Size: 2 (curve)
+   - Gradient from Red to Purple.
+
+2. Emission
+   - Rate over Time: 40
+   - Burst: time (0) count (20)
+
+3. Shape
+   - Shape set to Donut.
+   - Donut Radius changed to 0.2.
+   - Scaling Mode: Shape.
+
+4. Noise
+   - Noise enabled.
+   - Strength: 1.
+   - Frequency; 0.8.
+   - Scroll Speed: 0.5.
+
+5. Size by Speed
+   - Size: Curve.
+   - Range: 0-1.
+*/
---
+++ aaverett_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,28 @@
+/*
+UserInterface modifications for aaverett_Hour14
+
+1. Background
+   - Replaced the Hour 14 demo background with red landscape image.
+   - Set the image as a full-screen.
+   - Added a second Panel on top of it as a black overlay.
+   - Overlay Panel uses full stretch anchors.
+
+2. Title Text ("SOLAR DIVIDE")
+   - Converted to TextMeshPro.
+   - Increased size and placed it in the upper-left of the menu panel.
+   - Applied softness for highlighting effect.
+
+3. Description Text
+   - Updated text to: 
+        "The sun is failing. 
+         Civilizations turn on each other 
+         as the last light burns out."
+   - Increased size and placed it in the upper-left of the menu panel.
+   - Positioned title to left alignment.
+
+4. Start Button
+   - Button background changed to a near-black color with red text to fit theme.
+   - Changed button hightlight, normal, and selected options.
+   - No image sprite used for the button—only color.
+   - Position centered near the bottom of the menu panel.
+*/
