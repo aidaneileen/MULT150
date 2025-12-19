beb4bca35495fbb3f244eca56a96a9611ecbdea9
straysheep14 <aidaneileen123@gmail.com>
2025-12-18 20:52:45 -0800

    Final Complete

---
+++ aaverett_Final/Assets/Modifications.cs
@@ -0,0 +1,81 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: 
+            Changed light color to purple
+            Increased intensity to 1.3
+            Changed shadow type from soft shadow to hard shadow
+
+        2 - fog
+        modifications: 
+            Fog: ON
+            Mode: Linear
+            Color: soft purple
+            Start Distance: 15
+            End Distance: 75
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: 
+            Created a custom procedural skybox material
+            Set sky tint to purple
+            Set ground color to dark blue
+            Adjusted atmosphere thickness to 1.3
+            Increased exposure to 1.1
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: 
+            Created "Obstacle Light" in Obstacle prefab
+            Set range from 10 to 12
+            Changed color to red to match object color
+            Changed intensity from 1 to 2
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications: 
+            Replaced the original powerup model with a custom orb design made from:
+                4 spheres
+                2 cylinders
+                1 cube core
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary]
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        https://assetstore.unity.com/packages/2d/textures-materials/free-stylized-textures-rpg-environment-204187
+        texture source 1: Sides - Ice_3
+        texture source 2: Core - Snow_4
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: Changed color to neon blue
+        parameter 2: Changed shape from sphere to cone (shape)
+        parameter 3: Changed burst from 18 to 5 count (emission)
+        parameter 3: Changed angle from 25 to 10 (shape)
+
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
---
+++ aaverett_Final/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextureScroller ground;
+    public float gameTime = 10;
+
+    float totalTimeElasped = 0;
+    bool isGameOver = false;
+
+    void Update()
+    {
+        if (isGameOver)
+        return;
+
+        totalTimeElasped += Time.deltaTime;
+        gameTime -= Time.deltaTime;
+
+        if (gameTime <=0 )
+        SlowWorldDown();
+
+        if(gameTime<=0)
+        isGameOver = true;
+    }
+
+    public void AdjustTime(float amount)
+    {
+        gameTime += amount;
+        if (amount < 0)
+        SlowWorldDown();
+    }
+
+    void SlowWorldDown()
+    {
+        // Cancel any invokes to speed the world up
+        // Then slow the world down for 1 second
+        CancelInvoke();
+        Time.timeScale = 0.5f;
+        Invoke("SpeedWorldUp", 1);
+    }
+
+    void SpeedWorldUp()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Note this is using Unity's legacy GUI system
+    void OnGUI()
+    {
+        if(!isGameOver)
+        {
+            Rect boxRect = new Rect(Screen.width / 2 - 50, Screen.height - 100, 100, 50);
+            GUI.Box(boxRect, "Time Remaining");
+
+            Rect labelRect = new Rect(Screen.width / 2 - 10, Screen.height - 80, 20, 40);
+            GUI.Label(labelRect, ((int)gameTime).ToString());
+        }
+        else
+        {
+            Rect boxRect = new Rect(Screen.width / 2 - 60, Screen.height - 100, 120, 50);
+            GUI.Box(boxRect, "Game Over");
+
+            Rect labelRect = new Rect(Screen.width / 2 - 55, Screen.height - 80, 90, 40);
+            GUI.Label(labelRect, "Total Time: " + (int)totalTimeElasped);
+
+            Time.timeScale = 0;
+        }
+    }
+}
